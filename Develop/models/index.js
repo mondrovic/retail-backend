abf659cd75ec52f670f837021e09a0ff4cc7848e
Matthew O <mo@pop-os.localdomain>
2020-10-24 17:57:33 -0700

    setup model assoc

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,32 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "test",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "test",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: "test",
+  foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: "test",
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
