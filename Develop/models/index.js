2e71e70b8e64e2885bb4ee1bf07f5208e28d3c3e
Matthew O <mo@pop-os.localdomain>
2020-10-25 10:39:14 -0700

    set up model assoc

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,25 +6,23 @@ const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: "test",
+  foreignKey: "category_id",
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "test",
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: "test",
   foreignKey: "product_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: "test",
   foreignKey: "tag_id",
 });
 
